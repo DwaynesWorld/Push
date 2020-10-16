c49df76f12921bee1bd38b5c6e9cd5c18981be47
DwaynesWorld <kthompson713@gmail.com>
2020-10-16 16:23:31 -0500

    first commit

---
+++ src/push-ios/Pods/Target Support Files/Pods-Push-PushUITests/Pods-Push-PushUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Push_PushUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Push_PushUITests
+@end
---
+++ src/push-ios/Push/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Push
+//
+//  Created by Kyle Thompson on 10/16/20.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
